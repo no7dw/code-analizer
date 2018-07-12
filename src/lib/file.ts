eb7f85b89e6a072cbfcee571ffe8038655af66e2
no7dw <no7david@gmail.com>
2018-07-13 02:43:22 +0800

    first init

---
+++ src/lib/file.ts
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+
+class FileUtil {
+  /**
+   * 读取某个目录下所有文件名
+   */
+  static readFiles (dir) {
+    const files = fs.readdirSync(dir)
+    let allFiles = []
+    for (const file of files) {
+      const filename = path.resolve(dir, file)
+      const stat = fs.statSync(filename)
+      let theFiles = []
+      if (stat && stat.isDirectory()) {
+        theFiles = this.readFiles(filename)
+      } else {
+        theFiles = [filename]
+      }
+      allFiles = allFiles.concat(theFiles)
+    }
+    return allFiles
+  }
+}
+
+module.exports = FileUtil
